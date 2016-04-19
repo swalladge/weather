8c72b705f619bce3839ab7edda1d1461a40585c2
Samuel Walladge <samuel@swalladge.id.au>
2016-04-19 15:03:36 +0930

    added needed swing components, setup layout

--- src/FrontEnd/GUI.java
+++ src/FrontEnd/GUI.java
@@ -16,6 +16,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 
+import static sun.awt.X11.XConstants.buttons;
+
 
 /**
  * The user interface class - this sets up the gui, connects to the database, adds the listeners, etc.
@@ -23,55 +25,73 @@ import java.util.Comparator;
 public class GUI implements ActionListener {
 
     JFrame frame = new JFrame();
-    JPanel panel = new JPanel();
-    JPanel bottom = new JPanel();
+    JPanel drawPanel = new JPanel();
+    JPanel searchPanel = new JPanel();
     JButton loadButton = new JButton("Load");
     JButton displayButton = new JButton("Display");
-    JScrollPane scrollPane;
+    JButton searchButton = new JButton("Search");
+    JButton animateButton = new JButton("Animate!");
+    JScrollPane scrollPane = new JScrollPane();
     JTable dataTable;
     JEditorPane displayText;
-
+    JTextField searchText = new JTextField();
     Database db = null;
 
     public GUI() {
         // add event listener to buttons
         loadButton.addActionListener(this);
         displayButton.addActionListener(this);
+        animateButton.addActionListener(this);
+        searchButton.addActionListener(this);
 
-        scrollPane = new JScrollPane();
-
+        // main display output and weather table
         displayText = new JEditorPane();
         displayText.setContentType("text/html");
         displayText.setText("<h2>Welcome to the weather observation viewer!</h2><h3>Press <i>load</i> to load the weather data, then <i>display</i> to view it!</h3>");
         displayText.setEditable(false);
-
         scrollPane.setViewportView(displayText);
+        
+        // search panel consisting of a text box and button
+        searchPanel.setAlignmentX(1);
+        searchPanel.setLayout(new BoxLayout(searchPanel, BoxLayout.X_AXIS));
 
-        // bottom panel
-        bottom.setAlignmentX(1);
-        bottom.setLayout(new BoxLayout(bottom, BoxLayout.X_AXIS));
+        searchText.setColumns(30);
 
-        // add button and buffer
-        bottom.add(loadButton);
-        bottom.add(Box.createRigidArea(new Dimension(15, 0)));
-        bottom.add(displayButton);
-        bottom.add(Box.createRigidArea(new Dimension(15, 0)));
+        searchPanel.add(searchText);
+        searchPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        searchPanel.add(searchButton);
 
-        // panel
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        // setup the drawing panel
+        Dimension d = new Dimension(500, 500);
+        drawPanel.setPreferredSize(d);
+        // TODO: setup drawing stuff, timers, load images, etc.
 
-        // add stuff to panel
-        // panel.add(TODO);
-        panel.add(scrollPane);
-        panel.add(Box.createRigidArea(new Dimension(0, 15)));
-        panel.add(bottom);
-        panel.add(Box.createRigidArea(new Dimension(0, 15)));
+        // we have a layout now
+        SpringLayout layout = new SpringLayout();
+        Container content = frame.getContentPane();
+        content.setLayout(layout);
+
+        // add all the components
+        content.add(scrollPane);
+        content.add(searchPanel);
+        content.add(drawPanel);
+        content.add(loadButton);
+        content.add(displayButton);
+
+        // line up everything with spring layout
+        layout.putConstraint(SpringLayout.NORTH, searchPanel, 10, SpringLayout.SOUTH, scrollPane);
+        layout.putConstraint(SpringLayout.EAST, searchPanel, 0, SpringLayout.EAST, scrollPane);
+        layout.putConstraint(SpringLayout.WEST, drawPanel, 10, SpringLayout.EAST, scrollPane);
+        layout.putConstraint(SpringLayout.NORTH, loadButton, 10, SpringLayout.SOUTH, drawPanel);
+        layout.putConstraint(SpringLayout.WEST, loadButton, 10, SpringLayout.EAST, scrollPane);
+        layout.putConstraint(SpringLayout.WEST, displayButton, 10, SpringLayout.EAST, scrollPane);
+        layout.putConstraint(SpringLayout.NORTH, displayButton, 10, SpringLayout.SOUTH, loadButton);
 
         // frame
+        frame.pack();
         frame.setTitle("Weather Observations");
-        frame.setSize(600, 400);
+        frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(panel);
 
     }
 
@@ -85,6 +105,7 @@ public class GUI implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
+        // TODO: add actions for search and animate toggle buttons
         if (actionEvent.getActionCommand() == "Load") {
             db.loadObservationsFromHTMLFile();
             this.loadButton.setText("Loaded");
